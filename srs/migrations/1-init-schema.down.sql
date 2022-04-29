fea42c4e442136700cc360003bb6fd21c69ed6b1
Levi Gruspe <mail.levig@gmail.com>
2022-04-29 22:17:57 +0300

    Add table of coefficients for updating review intervals

--- srs/migrations/1-init-schema.down.sql
+++ srs/migrations/1-init-schema.down.sql
@@ -2,6 +2,10 @@ BEGIN TRANSACTION;
 PRAGMA user_version = 0;
 
 DROP VIEW MostRecentReview;
+
+DROP TRIGGER insert_default_coefficient;
+
 DROP TABLE Review;
+DROP TABLE Coefficient;
 
 COMMIT;
--- srs/migrations/1-init-schema.up.sql
+++ srs/migrations/1-init-schema.up.sql
@@ -1,14 +1,31 @@
 BEGIN TRANSACTION;
 PRAGMA user_version = 1;
 
+-- Table of coefficients, used to update intervals after reviewing.
+CREATE TABLE Coefficient (
+	streak INTEGER PRIMARY KEY,
+	coefficient FLOAT NOT NULL DEFAULT 2.0,
+	updated NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table of review sessions.
+-- Unseen items (newly learned words) won't appear here.
 CREATE TABLE Review (
 	word TEXT NOT NULL,
 	reviewed NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	correct BOOLEAN NOT NULL,		-- Result of this review.
 
-	interval INTEGER NOT NULL,
-	due NOT NULL DEFAULT,
-	correct BOOLEAN NOT NULL
+	interval INTEGER NOT NULL,	-- Used to compute next due date.
+	due NOT NULL,								-- Date of next review.
+
+	streak INTEGER REFERENCES Coefficient
 );
+
+CREATE TRIGGER insert_default_coefficient BEFORE INSERT ON Review
+WHEN NEW.streak NOT IN (SELECT streak FROM Coefficient)
+	BEGIN
+		INSERT INTO Coefficient (streak, coefficient) VALUES (NEW.streak, 2.0);
+	END;
 
 CREATE VIEW MostRecentReview AS
 SELECT DISTINCT A.word, A.due, A.interval, A.reviewed, A.correct
