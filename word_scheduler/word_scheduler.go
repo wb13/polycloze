6b637ac773495f0f66578074aeba8587f7524ec4
Levi Gruspe <mail.levig@gmail.com>
2022-05-12 17:37:58 +0300

    Add word_scheduler constructor

--- word_scheduler/word_scheduler.go
+++ word_scheduler/word_scheduler.go
@@ -3,10 +3,35 @@ package word_scheduler
 
 import (
 	"database/sql"
+	"path"
 
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/lggruspe/polycloze-srs/database"
 	rs "github.com/lggruspe/polycloze-srs/review_scheduler"
 	"github.com/lggruspe/polycloze-srs/word_queue"
 )
+
+// Returns in-memory sqlite DB, and attaches specified databases.
+func New(reviewDB, languageDB string) (*sql.DB, error) {
+	migrations := path.Join("migrations", "review_scheduler")
+	if err := database.UpgradeFile(reviewDB, migrations); err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := database.Attach(db, "review", reviewDB); err != nil {
+		return nil, err
+	}
+	if err := database.Attach(db, "language", languageDB); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
 
 // NOTE Only returns new words if words for review < n.
 // Expects language DB and review DB to already be attached.
