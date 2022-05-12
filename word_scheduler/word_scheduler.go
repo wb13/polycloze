c167609c3b06be64e855aefd3970a24c6f4df926
Levi Gruspe <mail.levig@gmail.com>
2022-05-12 17:31:36 +0300

    Include reviews in word_scheduler result

--- word_scheduler/word_scheduler.go
+++ word_scheduler/word_scheduler.go
@@ -4,12 +4,20 @@ package word_scheduler
 import (
 	"database/sql"
 
-	_ "github.com/lggruspe/polycloze-srs/review_scheduler"
+	rs "github.com/lggruspe/polycloze-srs/review_scheduler"
 	"github.com/lggruspe/polycloze-srs/word_queue"
 )
 
 // NOTE Only returns new words if words for review < n.
 // Expects language DB and review DB to already be attached.
 func GetWords(db *sql.DB, n int) ([]string, error) {
-	return word_queue.GetNewWords(db, n)
+	reviews, err := rs.ScheduleReviewNow(db, n)
+	if err != nil {
+		return nil, err
+	}
+	words, err := word_queue.GetNewWords(db, n-len(reviews))
+	if err != nil {
+		return nil, err
+	}
+	return append(reviews, words[:]...), nil
 }
