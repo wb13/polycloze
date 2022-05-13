18cbe5036c8d6ac870fbea4a646f980904f907c4
Levi Gruspe <mail.levig@gmail.com>
2022-05-13 13:57:15 +0300

    Fix main.go

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 
@@ -15,16 +18,18 @@ import (
 func readInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	return text
+	return strings.TrimSpace(text)
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tr, err := translator.NewTranslator(db, "spa.db", "eng.db", "translation.db")
+	tr, err := translator.NewTranslator(db, "spa.db", "eng.db", "translations.db")
 	if err != nil {
 		log.Fatal(err)
 	}
