23ca70167a84064a7d7aa0d4f0ff24c4bac76fac
Levi Gruspe <mail.levig@gmail.com>
2022-05-14 19:31:50 +0300

    Add 'Session' struct

--- database/session.go
+++ database/session.go
@@ -6,10 +6,22 @@ import (
 	"database/sql"
 )
 
+type Session struct {
+	con *sql.Conn
+}
+
+func (s *Session) Query(query string, args ...any) (*sql.Rows, error) {
+	return s.con.QueryContext(context.TODO(), query, args...)
+}
+
+func (s *Session) Begin() (*sql.Tx, error) {
+	return s.con.BeginTx(context.TODO(), nil)
+}
+
 // Returns a connection with the necessary attached databases.
 //
 // NOTE Caller is expected to close the connection after use.
-func NewSession(db *sql.DB, l1db, l2db, translationDb string) (*sql.Conn, error) {
+func NewSession(db *sql.DB, l1db, l2db, translationDb string) (*Session, error) {
 	ctx := context.TODO()
 	con, err := db.Conn(ctx)
 	if err != nil {
@@ -25,5 +37,5 @@ func NewSession(db *sql.DB, l1db, l2db, translationDb string) (*sql.Conn, error)
 	if err := attach(con, "translation", translationDb); err != nil {
 		return nil, err
 	}
-	return con, nil
+	return &Session{con: con}, nil
 }
