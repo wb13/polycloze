c312c77904bb0c33c09fbdb862c9f825098617d7
Levi Gruspe <mail.levig@gmail.com>
2022-05-14 20:03:08 +0300

    Remove unneeded constructor in word_scheduler

--- word_scheduler/word_scheduler.go
+++ word_scheduler/word_scheduler.go
@@ -2,34 +2,10 @@
 package word_scheduler
 
 import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-
 	"github.com/lggruspe/polycloze/database"
 	rs "github.com/lggruspe/polycloze/review_scheduler"
 	"github.com/lggruspe/polycloze/word_queue"
 )
-
-// Returns in-memory sqlite DB, and attaches specified databases.
-func New(reviewDB, l2db string) (*sql.DB, error) {
-	if err := database.UpgradeFile(reviewDB); err != nil {
-		return nil, err
-	}
-
-	db, err := sql.Open("sqlite3", ":memory:")
-	if err != nil {
-		return nil, err
-	}
-
-	if err := database.Attach(db, "review", reviewDB); err != nil {
-		return nil, err
-	}
-	if err := database.Attach(db, "l2", l2db); err != nil {
-		return nil, err
-	}
-	return db, nil
-}
 
 // NOTE Only returns new words if words for review < n.
 func GetWords(s *database.Session, n int) ([]string, error) {
