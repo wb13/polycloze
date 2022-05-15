91d51b5c3e4c799228c1cde567404bd441863bf5
Levi Gruspe <mail.levig@gmail.com>
2022-05-15 17:53:13 +0300

    Remove unused info table from schema

--- migrations/1-init-schema.sql
+++ migrations/1-init-schema.sql
@@ -1,14 +1,5 @@
 begin transaction;
-
 	pragma user_version = 1;
-
-	-- language metadata (only has one column)
-	create table info (
-		id integer primary key default 0,
-		code not null,
-		language not null,	-- only one language per database
-		check (id = 0)
-	);
 
 	create table word (
 		id integer primary key,
