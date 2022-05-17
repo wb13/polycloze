c17ea7a529cc36d24ee85965802eb8ad9b5a21bc
Levi Gruspe <mail.levig@gmail.com>
2022-05-17 11:03:52 +0300

    Show throughput in cmd/flashcards

--- cmd/flashcards/main.go
+++ cmd/flashcards/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/lggruspe/polycloze/database"
@@ -11,6 +13,15 @@ import (
 )
 
 func main() {
+	n := 10
+	var err error
+	if len(os.Args) >= 2 {
+		n, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			n = 10
+		}
+	}
+
 	rand.Seed(time.Now().UnixNano())
 
 	db, err := database.New("review.db")
@@ -24,8 +35,13 @@ func main() {
 		"../translations.db",
 	)
 
-	items := ig.GenerateItems(10)
+	start := time.Now()
+
+	items := ig.GenerateItems(n)
 	for _, item := range items {
 		fmt.Println(item)
 	}
+
+	throughput := float64(len(items)) / time.Since(start).Seconds()
+	fmt.Printf("throughput: %v\n", throughput)
 }
