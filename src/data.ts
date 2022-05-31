7f9b3ee7f5bf5feb45c7cf2c5cc2f500dc43e3e9
Levi Gruspe <mail.levig@gmail.com>
2022-05-31 12:03:40 +0300

    Add function for posting Reviews

--- src/data.ts
+++ src/data.ts
@@ -6,3 +6,24 @@ export async function fetchItems (src: string): Promise<Item[]> {
   const json = await response.json()
   return json.items
 }
+
+type Review = {
+  word: string
+  correct: boolean
+}
+
+export async function submitReviews (src: string, reviews: Review[]): Promise<boolean> {
+  const options = {
+    body: JSON.stringify({ reviews }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    mode: 'cors'
+  }
+  const request = new Request(src, options)
+  const response = await fetch(request)
+  const json = await response.json()
+  return json.success
+}
