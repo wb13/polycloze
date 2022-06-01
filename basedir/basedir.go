550597717af5459419f0dcd48670e8209344e454
Levi Gruspe <mail.levig@gmail.com>
2022-06-01 12:09:48 +0300

    Add basedir package

---
+++ basedir/basedir.go
@@ -0,0 +1,70 @@
+package basedir
+
+import (
+	"log"
+	"os"
+	"path"
+)
+
+var (
+	Home string
+	DataDir string
+	StateDir string
+)
+
+func init() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	Home = home
+}
+
+func xdgDataHome() string {
+	val := os.Getenv("XDG_DATA_HOME")
+	if val != "" {
+		return val
+	}
+	return path.Join(Home, ".local", "share")
+}
+
+func xdgStateHome() string {
+	val := os.Getenv("XDG_STATE_HOME")
+	if val != "" {
+		return val
+	}
+	return path.Join(Home, ".local", "state")
+}
+
+func initDataDir() error {
+	DataDir = path.Join(xdgDataHome(), "polycloze")
+	languages := path.Join(DataDir, "languages")
+
+	if err := os.MkdirAll(languages, 0700); err != nil {
+		DataDir = ""
+		return err
+	}
+	return nil
+}
+
+func initStateDir() error {
+	StateDir = path.Join(xdgStateHome(), "polycloze")
+	user := path.Join(StateDir, "user")
+
+	if err := os.MkdirAll(user, 0700); err != nil {
+		StateDir = ""
+		return err
+	}
+	return nil
+}
+
+// NOTE This function is different from init.
+func Init() error {
+	if err := initDataDir(); err != nil {
+		return err
+	}
+	if err := initStateDir(); err != nil {
+		return err
+	}
+	return nil
+}
