c551a8a68e3b0380a13bb3dc46b793b0e30dbae8
Levi Gruspe <mail.levig@gmail.com>
2022-06-04 15:22:51 +0300

    Show language selector in main page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import './index.css'
 import { createApp } from './app'
+import { supportedLanguages } from './data'
+import { createLanguageSelectForm } from './select'
+
+window.onload = () => {
+  supportedLanguages().then(languages => {
+    const form = createLanguageSelectForm(languages)
+    document.body.appendChild(form)
+  })
+}
 
 createApp([])
   .then(([app, ready]) => {
