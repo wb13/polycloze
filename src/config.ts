dbbc720433cd3fefbea854943dae5b796a527d2f
Levi Gruspe <mail.levig@gmail.com>
2022-06-04 15:21:45 +0300

    Add language select element

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,28 @@
-// Compile-time config
-
 // Get location of server, but with a different port number.
 function getLocation (port: string): string {
-  const { protocol, hostname, pathname, search, hash } = location
-  return `${protocol}//${hostname}:${port}${pathname}${search}${hash}`
+  const url = new URL(location.href)
+  url.port = port
+  return url.href
 }
 
+// Location of server
 export const src = getLocation('3000')
+
+let l1 = 'eng'
+let l2 = 'spa'
+
+export function getL1 (): string {
+  return l1
+}
+
+export function getL2 (): string {
+  return l2
+}
+
+export function setL1 (code: string) {
+  l1 = code
+}
+
+export function setL2 (code: string) {
+  l2 = code
+}
---
+++ src/select.ts
@@ -0,0 +1,43 @@
+import './select.css'
+import { getL1, getL2, setL1, setL2 } from './config'
+
+type Language = {
+  code: string
+  native: string
+  english: string
+}
+
+function createLanguageOption (language: Language, selected: boolean = false): HTMLOptionElement {
+  const option = document.createElement('option')
+  option.value = language.code
+  option.selected = selected
+  option.textContent = language.native
+  return option
+}
+
+// NOTE Doesn't update config.l1 or l2.
+// You have to do it using the onChange callback function.
+function createLanguageSelectInput (languages: Language[], selected: string = 'spa', onChange: () => void = () => {}): HTMLSelectElement {
+  const select = document.createElement('select')
+  select.append(...languages.map(language => createLanguageOption(language, language.code === selected)))
+  select.addEventListener('change', onChange)
+  return select
+}
+
+export function createLanguageSelectForm (languages: Language[]): HTMLFormElement {
+  const form = document.createElement('form')
+
+  const rerender = () => form.replaceWith(createLanguageSelectForm(languages))
+
+  const l1 = createLanguageSelectInput(languages, getL1(), () => {
+    setL1(l1.value)
+    rerender()
+  })
+  const l2 = createLanguageSelectInput(languages.filter(l => l.code !== getL1()), getL2(), () => {
+    setL2(l2.value)
+    rerender()
+  })
+
+  form.append('🌐 ', l1, ' > ', l2)
+  return form
+}
