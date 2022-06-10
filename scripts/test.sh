80e9726e4025e0f0ed3f71971223972c457345d7
Levi Gruspe <mail.levig@gmail.com>
2022-06-10 12:24:41 +0300

    Update db test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,27 @@
 
 # For manual database testing
 
-sqlite3 -init ./scripts/test.sql
+if [[ "$1" == "" ]]; then
+	echo "missing L1 ISO 639-3 language code"
+	exit 1
+fi
+
+if [[ "$2" == "" ]]; then
+	echo "missing L2 ISO 639-3 language code"
+	exit 1
+fi
+
+sql=".read database/migrations/1_init_schema.up.sql
+
+attach database '$HOME/.local/share/polycloze/languages/$2.db' as l2;
+attach database '$HOME/.local/share/polycloze/languages/$1.db' as l1;
+attach database '$HOME/.local/share/polycloze/translations.db' as translation;
+
+.timer on
+"
+
+tmpfile="$(mktemp)"
+trap "rm -f '$tmpfile'" EXIT
+
+echo "$sql" > "$tmpfile"
+sqlite3 -init "$tmpfile"
--- scripts/test.sql
+++
@@ -1,7 +0,0 @@
-.read database/migrations/1_init_schema.up.sql
-
-attach database './cmd/spa.db' as l2;
-attach database './cmd/eng.db' as l1;
-attach database './cmd/translations.db' as translation;
-
-.timer on
