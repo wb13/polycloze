2b572eda7db529346a29dde4c8e7850126ea73ee
Levi Gruspe <mail.levig@gmail.com>
2022-06-13 16:43:25 +0300

    Add translation mapper

---
+++ scripts/mapper.py
@@ -0,0 +1,43 @@
+"""Maps translations."""
+
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from sys import exit
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("l1", type=Path, help="L1 sentences TSV file")
+    parser.add_argument("l2", type=Path, help="L2 sentences TSV file")
+    parser.add_argument("links", type=Path, help="Tatoeba links file")
+    return parser.parse_args()
+
+
+def get_ids(path: Path) -> set[str]:
+    with open(path, "r") as file:
+        return {line.split()[0] for line in file}
+
+
+def main() -> None:
+    args = parse_args()
+    if not args.l1.is_file():
+        exit(f"{args.l1!s} does not exist")
+    if not args.l2.is_file():
+        exit(f"{args.l2!s} does not exist")
+    if not args.links.is_file():
+        exit(f"{args.links!s} does not exist")
+
+    l1_ids = get_ids(args.l1)
+    l2_ids = get_ids(args.l2)
+
+    with open(args.links, "r") as file:
+        for line in file:
+            source, target = line.split()
+            if source in l1_ids and target in l2_ids:
+                print(f"{source},{target}")
+            elif source in l2_ids and target in l1_ids:
+                print(f"{target},{source}")
+
+
+if __name__ == "__main__":
+    main()
