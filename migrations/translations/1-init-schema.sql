3c7ffc90d0fd8259764fe13bdc0de24c6806cf22
Levi Gruspe <mail.levig@gmail.com>
2022-06-13 17:55:39 +0300

    Only create table and indices in translation db if they don't exist

--- migrations/translations/1-init-schema.sql
+++ migrations/translations/1-init-schema.sql
@@ -1,13 +1,13 @@
 begin transaction;
 	pragma user_version = 1;
 
-	create table translation (
+	create table if not exists translation (
 		l1,	-- references <l1.db>.sentence.tatoeba_id
 		l2	-- references <l2.db>.sentence.tatoeba_id
 	);
 
-	create index index_translation_l1 on translation (l1);
+	create index if not exists index_translation_l1 on translation (l1);
 
-	create index index_translation_l2 on translation (l2);
+	create index if not exists index_translation_l2 on translation (l2);
 
 	commit;
