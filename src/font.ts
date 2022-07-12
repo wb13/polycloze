77cda7c5de25186a3f3a7ce7114d3f780bf0f104
Levi Gruspe <mail.levig@gmail.com>
2022-07-12 17:12:47 +0300

    Annotate 2d context type

--- src/font.ts
+++ src/font.ts
@@ -9,7 +9,7 @@ export function getFont (ele: Element): string {
 }
 
 export function getWidth (font: string, text: string): string {
-    const context = canvas.getContext("2d")!;
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
     context.font = font;
     const metrics = context.measureText(text);
     return `${metrics.width}px`;
