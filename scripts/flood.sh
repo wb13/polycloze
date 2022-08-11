2a1f3350d5da878a3c3dbaffffc37b32a50307b2
Levi Gruspe <mail.levig@gmail.com>
2022-08-11 20:37:03 +0300

    Add flood script

---
+++ scripts/flood.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2022 Levi Gruspe
+# License: MIT, or APGLv3 or later
+
+duration="2"
+
+if [[ "$1" != "" ]]; then
+	duration="$1"
+fi
+
+while true; do
+	if ! curl http://localhost:3000/eng/spa; then
+		exit 1
+	fi
+	sleep "$duration"
+done
