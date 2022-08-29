18fa5b4e799f64b5e89919348463e104ae4dede0
Levi Gruspe <mail.levig@gmail.com>
2022-08-29 17:27:02 +0800

    Make partition function atomic

--- scripts/partition.py
+++ scripts/partition.py
@@ -18,10 +18,21 @@ def output_parsed_line(line: str, basedir: Path) -> None:
         print(f"{id_}\t{sentence}", file=outfile)
 
 
-def partition(inputfile: Path | None, basedir: Path) -> None:
-    with fileinput.input(files=inputfile or "-", encoding="utf-8") as file:
+def partition(inputfile: Path | None, out: Path) -> None:
+    """Extract sentences from inputfile.
+
+    Sentences are partitioned by language.
+    These are first written in a temp directory before being copied to the
+    output directory, so that output files are never half-finishsed.
+    """
+    with (
+        fileinput.input(files=inputfile or "-", encoding="utf-8") as file,
+        TemporaryDirectory() as tmpname,
+    ):
+        tmp = Path(tmpname)
         for line in file:
-            output_parsed_line(line, basedir)
+            output_parsed_line(line, tmp)
+        copytree(tmp, out, dirs_exist_ok=True)
 
 
 def parse_args() -> Namespace:
@@ -40,14 +51,8 @@ def main(args: Namespace) -> None:
     out = Path(args.out)
     if out.is_file():
         sys.exit("output file already exists and is not a directory")
-
-    if not is_outdated([args.out], [args.file]):
-        return
-
-    with TemporaryDirectory() as tmpname:
-        tmp = Path(tmpname)
-        partition(args.file, tmp)
-        copytree(tmp, out, dirs_exist_ok=True)
+    if is_outdated([args.out], [args.file]):
+        partition(args.file, out)
 
 
 if __name__ == "__main__":
