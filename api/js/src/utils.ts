0cf79b2518256299ffcd2a36304d1af8ffc55151
Levi Gruspe <mail.levig@gmail.com>
2022-10-29 16:41:02 +0800

    Insert form into DOM before submitting

--- api/js/src/utils.ts
+++ api/js/src/utils.ts
@@ -2,5 +2,9 @@ export function followLinkPost(url: string) {
     const form = document.createElement("form");
     form.action = url;
     form.method = "POST";
+    form.style.display = "none";
+
+    document.body.appendChild(form);
+
     form.submit();
 }
