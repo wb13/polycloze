36a95580e6da9c12ecc263d8d15cc7fa823f9626
Levi Gruspe <mail.levig@gmail.com>
2022-11-02 15:40:13 +0800

    Only generate one csrf token per session

--- sessions/csrf.go
+++ sessions/csrf.go
@@ -1,43 +1,26 @@
 // Copyright (c) 2022 Levi Gruspe
 // License: GNU AGPLv3 or later
 
-// Per-session anti-CSRF tokens.
 package sessions
 
 import (
-	"database/sql"
-	"fmt"
+	"crypto/sha256"
+	"encoding/base64"
 )
 
-// Returns a anti-CSRF token (connected to session in DB).
-func CreateCSRFToken(db *sql.DB, sessionID string) (string, error) {
-	token, err := generateID()
+// Creates CSRF token for session.
+// Input should be base64 encoded.
+func CSRFToken(sessionID string) string {
+	bytes, err := base64.StdEncoding.DecodeString(sessionID)
 	if err != nil {
-		return "", fmt.Errorf("failed to generate a CSRF token: %v", err)
+		panic(err)
 	}
-
-	query := `INSERT INTO csrf_token (session_id, token) VALUES (?, ?)`
-	if _, err = db.Exec(query, sessionID, token); err != nil {
-		return "", fmt.Errorf("failed to generate a CSRF token: %v", err)
-	}
-	return token, nil
-}
-
-// Deletes a CSRF token from the database.
-func DeleteCSRFToken(db *sql.DB, sessionID, token string) error {
-	query := `
-		DELETE FROM csrf_token WHERE rowid in (
-			SELECT min(rowid) AS rowid FROM csrf_token WHERE (session_id, token) = (?, ?)
-		)
-	`
-	_, err := db.Exec(query, sessionID, token)
-	return err
+	result := sha256.Sum256(bytes)
+	bytes = result[:]
+	return base64.StdEncoding.EncodeToString(bytes)
 }
 
 // Validates CSRF token.
-func CheckCSRFToken(db *sql.DB, sessionID, token string) bool {
-	var result string
-	query := `SELECT token FROM csrf_token WHERE (session_id, token) = (?, ?) LIMIT 1`
-	err := db.QueryRow(query, sessionID, token).Scan(&result)
-	return err == nil
+func CheckCSRFToken(sessionID, token string) bool {
+	return CSRFToken(sessionID) == token
 }
