4a70df6ace451379ed3d97cf30839bfdd6d70b94
Levi Gruspe <mail.levig@gmail.com>
2022-11-05 19:22:24 +0800

    Add pagination element constructor

--- api/js/src/button.ts
+++ api/js/src/button.ts
@@ -1,14 +1,14 @@
 import "./button.css";
 import { createCSRFTokenInput } from "./csrf";
 
-export function setButton(button: HTMLButtonElement, content: string, onClick?: (event: Event) => void) {
-    button.textContent = content;
+export function setButton(button: HTMLButtonElement, content: string | Element, onClick?: (event: Event) => void) {
+    button.append(content);
     if (onClick) {
         button.addEventListener("click", onClick);
     }
 }
 
-export function createButton(content: string, onClick?: (event: Event) => void): HTMLButtonElement {
+export function createButton(content: string | Element, onClick?: (event: Event) => void): HTMLButtonElement {
     const button = document.createElement("button");
     setButton(button, content, onClick);
     return button;
---
+++ api/js/src/pagination.ts
@@ -0,0 +1,68 @@
+import "./pagination.css";
+import { createButton } from "./button";
+
+function createTightButton(content: string | Element, onClick?: (event: Event) => void): HTMLButtonElement {
+    const button = createButton(content, onClick);
+    button.classList.add("button-borderless");
+    button.classList.add("button-tight");
+    return button;
+}
+
+function createPreviousButton(disabled: boolean, onClick?: (event: Event) => void): HTMLButtonElement {
+    const button = createTightButton("Prev", onClick);
+    button.disabled = disabled;
+    return button;
+}
+
+function createNextButton(disabled: boolean, onClick?: (event: Event) => void): HTMLButtonElement {
+    const button = createTightButton("Next", onClick);
+    button.disabled = disabled;
+    return button;
+}
+
+function createPageButton(page: number, selected: boolean, onClick?: (event: Event) => void): HTMLButtonElement {
+    if (!selected) {
+        return createTightButton(String(page), onClick);
+    }
+    const content = document.createElement("b");
+    content.textContent = String(page);
+    return createTightButton(content);
+}
+
+export function createPagination(page: number, lastPage: number): HTMLDivElement {
+    if (page <= 0) {
+        throw new Error(`page should be positive: ${page}`);
+    }
+    if (lastPage <= 0) {
+        throw new Error(`lastPage should be positive: ${lastPage}`);
+    }
+    if (page > lastPage) {
+        throw new Error(`page should be <= lastPage: ${page}, ${lastPage}`);
+    }
+    const children: Array<string | Element> = [createPreviousButton(page === 1)];
+
+    children.push(createPageButton(1, page === 1));
+    if (page >= 5) {
+        children.push("⋯");
+    } else if (page === 4) {
+        children.push(createPageButton(2, false));
+    }
+    for (let i = Math.max(2, page - 1); i <= Math.min(page + 1, lastPage); i++) {
+        children.push(createPageButton(i, page === i));
+    }
+    if (lastPage - page >= 4) {
+        children.push("⋯");
+    } else if (lastPage - page === 3) {
+        children.push(createPageButton(lastPage - 1, page === lastPage - 1));
+    }
+    if (lastPage - page >= 2) {
+        children.push(createPageButton(lastPage, page === lastPage));
+    }
+
+    children.push(createNextButton(page === lastPage));
+
+    const div = document.createElement("div");
+    div.classList.add("pagination");
+    div.append(...children);
+    return div;
+}
