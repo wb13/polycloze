7c9c373df013ed76ef5f8bf199291f874e9eac8d
Levi Gruspe <mail.levig@gmail.com>
2022-11-16 13:12:38 +0800

    Fix vocab chart labels

--- api/js/src/chart.ts
+++ api/js/src/chart.ts
@@ -24,23 +24,31 @@ Chart.register(
 Chart.defaults.font.family = "Nunito";
 
 const dayLabels = [
+    "Sun",
     "Mon",
     "Tue",
     "Wed",
     "Thu",
     "Fri",
     "Sat",
-    "Sun",
 ];
+
+// Returns date n days ago.
+function dateNDaysAgo(n: number): Date {
+    const daysSinceEpoch = Date.now() / 1000 / 60 / 60 / 24 - n;
+    return new Date(daysSinceEpoch * 24 * 60 * 60 * 1000);
+}
 
 // Returns vocabulary size data over the past week.
 function vocabularyData(activityHistory: Activity[]): ChartData {
-    const data = activityHistory.slice(0, 7).reverse().map(a => a.learned - a.forgotten);
+    const week = activityHistory.slice(0, 7);
+    const labels = week.map((_, i) => dayLabels[dateNDaysAgo(i).getDay()]).reverse();
+    const data = week.map(a => a.learned - a.forgotten).reverse();
     for (let i = 1; i < data.length; i++) {
         data[i] += data[i-1];
     }
     return {
-        labels: dayLabels,  // TODO fix order of labels
+        labels,
         datasets: [{data, fill: true}],
     };
 }
