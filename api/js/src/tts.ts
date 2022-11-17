b930454e5419441300b34a94446d692da0fc3f33
Levi Gruspe <mail.levig@gmail.com>
2022-11-17 12:46:13 +0800

    Reorder language codes for Tagalog TTS

--- api/js/src/tts.ts
+++ api/js/src/tts.ts
@@ -3,7 +3,7 @@ import { getL2 } from "./language";
 function fixLanguage(lang: string): string {
     // Speech synthesis voices sometimes don't use BCP47 language codes.
     if (lang === "tl") {
-        const choices = new Set(["tl", "tgl", "fil", "fil-PH"]);
+        const choices = new Set(["tl", "fil-PH", "tgl", "fil"]);
         for (const voice of speechSynthesis.getVoices()) {
             if (choices.has(voice.lang)) {
                 return voice.lang;
