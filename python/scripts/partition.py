c48bcf72d30afc8843761663f5e23d651ac77024
Levi Gruspe <mail.levig@gmail.com>
2022-11-18 23:31:33 +0800

    Speed-up sentence partition

--- python/scripts/partition.py
+++ python/scripts/partition.py
@@ -1,48 +1,91 @@
-#!/usr/bin/env python
-
-"""Partitions tatoeba sentences.tsv into multiple files (one per language)."""
+"""Partitions tatoeba sentences.tsv into separate files per language."""
 
 from argparse import ArgumentParser, Namespace
-import fileinput
 from pathlib import Path
+from shlex import quote
 from shutil import copytree
+from subprocess import CalledProcessError, run
 import sys
 from tempfile import TemporaryDirectory
 
-from .dependency import is_outdated
+
+class SortError(Exception):
+    """Raised when `sort` program fails."""
 
 
-def output_parsed_line(line: str, basedir: Path) -> None:
-    [id_, language, sentence] = line.strip().split("\t")
-    with open(basedir/f"{language}.tsv", "a", encoding="utf-8") as outfile:
-        print(f"{id_}\t{sentence}", file=outfile)
+def sort_file_lines(infile: Path, outfile: Path) -> None:
+    """Write sorted lines in `infile` to `outfile`.
+
+    May raise `SortError`.
+    """
+    with TemporaryDirectory() as tmpname:
+        sorted_file = Path(tmpname)/"sorted.csv"
+        args = [
+            "sort",
+            "-k2,2",
+            quote(str(infile.resolve())),
+            "-o",
+            quote(str(sorted_file.resolve())),
+        ]
+        try:
+            run(args, check=True)
+        except CalledProcessError as exc:
+            raise SortError from exc
+
+        sorted_file.replace(outfile)
 
 
-def partition(inputfile: Path | None, out: Path) -> None:
-    """Extract sentences from inputfile.
+def partition(infile: Path, outdir: Path) -> None:
+    """Extract sentences from `infile`.
 
     Sentences are partitioned by language.
     These are first written in a temp directory before being copied to the
     output directory, so that output files are never half-finished.
+
+    May raise `SortError`.
     """
-    with (
-        fileinput.input(files=inputfile or "-", encoding="utf-8") as file,
-        TemporaryDirectory() as tmpname,
-    ):
-        tmp = Path(tmpname)
-        for line in file:
-            output_parsed_line(line, tmp)
-        copytree(tmp, out, dirs_exist_ok=True)
+    with TemporaryDirectory() as tmpname:
+        # Sort by language, so all sentences in the same language are together.
+        sorted_file = Path(tmpname)/"sorted.csv"
+        staging = Path(tmpname)/"staging"
+        staging.mkdir()
+
+        sort_file_lines(infile, sorted_file)
+
+        with open(sorted_file, encoding="utf-8") as lines:
+            prev = None
+            file = None
+            for line in lines:
+                [id_, language, sentence] = line.strip().split("\t")
+
+                if language != prev:
+                    print(language)
+                    if file is not None:
+                        file.close()
+                    prev = language
+                    file = open(    # pylint: disable=consider-using-with
+                        staging/f"{language}.tsv",
+                        "a",
+                        encoding="utf-8",
+                    )
+
+                print(f"{id_}\t{sentence}", file=file)
+
+            if file is not None:
+                file.close()
+
+        copytree(staging, outdir, dirs_exist_ok=True)
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("out", help="output directory")
     parser.add_argument(
-            "-f",
-            dest="file",
-            type=Path,
-            help="sentences.csv file (default: stdin)",
+        "-f",
+        dest="file",
+        type=Path,
+        help="sentences.csv file",
+        required=True,
     )
     return parser.parse_args()
 
@@ -50,9 +93,8 @@ def parse_args() -> Namespace:
 def main(args: Namespace) -> None:
     out = Path(args.out)
     if out.is_file():
-        sys.exit("output file already exists and is not a directory")
-    if is_outdated([args.out], [args.file]):
-        partition(args.file, out)
+        sys.exit("destination already exists as a file")
+    partition(args.file, out)
 
 
 if __name__ == "__main__":
