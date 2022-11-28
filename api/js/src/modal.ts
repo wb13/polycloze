00d461af14ae5d47eca93cdfb61c74b3fe13163b
Levi Gruspe <mail.levig@gmail.com>
2022-11-28 11:17:34 +0800

    Prevent page from shifting when scrollbar disappears

--- api/js/src/modal.ts
+++ api/js/src/modal.ts
@@ -45,16 +45,50 @@ function createModalBackground(hide: () => void): HTMLDivElement {
     return div;
 }
 
+const styleReset: Array<() => void> = [];
+
 // Used for showing modal elements only.
 function showModalElement(element: HTMLElement) {
     element.classList.remove("modal-hidden");
+
+    // Save original style values.
+    const style = {
+        overflow: document.body.style.overflow,
+        paddingRight: document.body.style.paddingRight,
+        width: document.body.style.width,
+    };
+
+    // Prevent whole page from shifting to the right when scroll bar disappears.
+    // Idea: widen the document body to fill the space left behind by the scrollbar.
+    const htmlWidth = document.documentElement.clientWidth;
+    const windowWidth = window.innerWidth;
+    const scrollbarWidth = windowWidth - htmlWidth;
+    if (scrollbarWidth > 0) {
+        document.body.style.paddingRight = Math.floor(scrollbarWidth) + "px";
+        document.body.style.width = `calc(100vw - ${Math.floor(scrollbarWidth)}px)`;
+    }
+
+    // Prevent scrolling.
     document.body.style.overflow = "hidden";
+
+    // Function that undoes CSS changes in this function.
+    const reset = () => {
+        const {overflow, paddingRight, width} = style;
+        document.body.style.overflow = overflow;
+        document.body.style.paddingRight = paddingRight;
+        document.body.style.width = width;
+    };
+    styleReset.push(reset);
 }
 
 // Used for hiding modal elements only.
 function hideModalElement(element: HTMLElement) {
     element.classList.add("modal-hidden");
     document.body.style.overflow = "";
+    const reset = styleReset.pop();
+    if (reset != null) {
+        reset();
+    }
 }
 
 type CreateModalOptions = {
